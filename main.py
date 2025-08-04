90d0d513e7dd0c2a043db8a1bfb851394c240613
renan-dutra-fsae <renan-dutra97@poli.ufrj.br>
2025-08-04 14:35:13 -0300

    Added main.py
    Apenas testando o git e adicionando um arquivo inicial

---
+++ main.py
@@ -0,0 +1,35 @@
+"""
+DV-Sim Library
+--------------
+
+Descrição:
+    Biblioteca básica para o projeto DV-Sim.
+
+Autor: Renan
+Data: 04/08/2025
+"""
+
+__version__ = "0.1.0"
+__author__ = "Renan"
+
+class DvSim:
+    """
+    Classe principal da biblioteca DV-Sim.
+    """
+
+    def __init__(self):
+        """
+        Inicializa a classe DvSim.
+        """
+        pass
+
+    def exemplo(self):
+        """
+        Método de exemplo.
+        """
+        print("DV-Sim está funcionando!")
+
+# Exemplo de uso
+if __name__ == "__main__":
+    sim = DvSim()
+    sim.exemplo()
